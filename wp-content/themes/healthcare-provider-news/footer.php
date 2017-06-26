1689124f3856014b59e8ccd55340571de18028cf
Adam Meyer <dev@apmeyer.com>
2017-06-26 11:25:01 -0500

    Add Auth0 badge to footer
    The display of the badge is conditional based on whether or not the main Auth0 plugin class exists.

--- wp-content/themes/healthcare-provider-news/footer.php
+++ wp-content/themes/healthcare-provider-news/footer.php
@@ -12,6 +12,11 @@
         </div>
       </div>
     </div>
+    <?php if ( class_exists('WP_Auth0') ) { ?>
+      <div class="footer-badge">
+        <img src="<?php echo get_template_directory_uri() ?>/assets/dist/img/a0-badge-light.png" alt="Token Based Authentication from Auth0" height="100" width="300" />
+      </div>
+    <?php } ?>
   </footer>
 
   <?php wp_footer(); ?>
